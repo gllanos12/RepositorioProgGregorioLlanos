9326c15dc0d9a946c5800a6db41e1a9fd624da0a
Gregorio Llanos <gllanos@unal.edu.co>
2021-10-15 03:17:19 +0000

    commit programa 1 final

--- semana2/programa01.cpp
+++ semana2/programa01.cpp
@@ -1,16 +1,32 @@
-// Calcular los numeros primos entre 1 y 100
+// Calcular los numeros pares entre 1 y 100
 
 // incluir librerias que vamos a usar
 #include <iostream>
 #include <cmath>
 
+void print_even(int nmin, int nmax);//declaracion
+
 // main function
 int main(void)
 {
-  int m=1;
-  int n=100;
+  const int m=1;
+  const int n=100;
   std::cout << "m:" << m << "\n";
   std::cout << "n:" << n << "\n";
 
+  print_even(m, n);
+
   return 0;
+}
+void print_even(int nmin, int nmax)
+{
+  for(int ii=nmin ; ii <= nmax ; ii = ii+1)
+  {
+    
+    if(ii%2==0)
+    {
+      std::cout << ii << "\n";
+    }
+  }
+  return;
 }
