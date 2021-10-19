9510bca43fe03d896fad85e7648dfee476ffe7f9
Gregorio Llanos <gllanos@unal.edu.co>
2021-10-18 21:01:49 -0500

    commit variables

---
+++ 15-10-2021/programa2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello world" << "\n";
+
+  int a;
+  float x;
+  double y=0.0;
+  std::cout << " a = " << a << "\n";
+  std::cout << " x = " << x << "\n";
+  std::cout << " y = " << y << "\n";
+  return 0;
+}
